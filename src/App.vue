ad22d99d7fd378f8009fb0866253ef51440d20e7
davidllona <dllonaProgramador@gmail.com>
2023-02-16 00:06:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <RickAndMortyCharacters />
+  </div>
+</template>
+
+<script>
+import RickAndMortyCharacters from './components/RickAndMortyCharacters.vue';
+
+export default {
+  components: {
+    RickAndMortyCharacters,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
